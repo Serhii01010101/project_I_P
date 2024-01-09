4f67c9dd50bfc9fc4be9c26febd7c6431e65bb21
Serhii01010101 <jobjob2109@gmail.com>
2024-01-09 21:47:49 +0200

    lesson 1

--- js/script.js
+++ js/script.js
@@ -1,107 +1,44 @@
-// let number = 4.6;
+/* Задание на урок:
 
-// console.log(4 / 0);
-// console.log('string' * 9);
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// const persone = 'Alex';
-// const person2 = `Alex`;
 
-// const persone3 = '5';
 
-// const bool = true;
-// const isOpen = false;
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// console.log(something);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-// const obj = {
-//   name: 'John',
-//   age: 25,
-//   isMarried: false,
-// };
+Проверить, чтобы все работало без ошибок в консоли */
 
-// // console.log(obj.name);
-// console.log(obj['name']);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-// console.log(arr[1]);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// const arr = [1, 2, 3]  массив- это перечень информации по порядку
-// // т.е у каждой сущности есть определенный номер по порядку
-// const arr = ['a', 'b', 'c'];
-// const arrObj = {
-//   a: 'a',
-//   1: 'b',
-//   2: 'c',
-// };
-// const b = 'b';
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
 
-// arrObj.b = '12345';
-// console.log(arrObj.b);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// console.log(arr[1]);
-// console.log(arrObj[1]);
-
-// // const obj = { a:1, b:2} объект
-
-// const obj = {
-//   Anna: 500,
-//   Alice: 800,
-// };
-
-// const storeName = 'Rozetka';
-// const storeDescription = {
-//   budget: 10000,
-//   employees: ['Tom', 'Jerry', 'Alex'],
-//   products: {
-//     phone: 1000,
-//     ipad: 700,
-//   },
-//   open: true,
-// };
-
-// alert('Hello');
-
-// const result = confirm('Are you here?');
-// console.log(result);
-
-// const answer = +prompt('Are you 18 years old?', '18');
-// console.log(answer + 10);
-
-// const answers = [];
-// answers[0] = prompt('what is your first name?', '');
-// answers[1] = prompt('what is your last name?', '');
-// answers[2] = prompt('what old are you?', '');
-
-// console.log(typeof answers);
-
-// const category = 'toys';
-
-// // console.log(' https://someurl.com/' + category + '/' + '4' );
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user = 'Ivan';
-// alert(`Hello, ${user}`);
-
-// console.log('arr' + '- object');
-// console.log(4 + +'- object');
-
-// console.log(4 + +'5');
-
-// let incr = 10,
-//   decr = 10;
-
-// // ++incr;
-// // --decr;
-
-// // console.log(++incr);
-// // console.log(--decr);
-
-// // console.log(5 % 2);
-
-// console.log(2 + 2 * 2 !== '6');
-
-// // const isChecked = false;
-// // isClose = false;
-
-// // console.log(isChecked || !isClose);
+console.log(personalMovieDB);
