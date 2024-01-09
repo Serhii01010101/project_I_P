a1c01ab1de6c1996473ae0646f86bfcf134ffbea
Serhii <jobjob2109@gmail.com>
2024-01-09 20:58:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,107 @@
+// let number = 4.6;
+
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+// const person2 = `Alex`;
+
+// const persone3 = '5';
+
+// const bool = true;
+// const isOpen = false;
+
+// console.log(something);
+
+// const obj = {
+//   name: 'John',
+//   age: 25,
+//   isMarried: false,
+// };
+
+// // console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const arr = [1, 2, 3]  массив- это перечень информации по порядку
+// // т.е у каждой сущности есть определенный номер по порядку
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//   a: 'a',
+//   1: 'b',
+//   2: 'c',
+// };
+// const b = 'b';
+
+// arrObj.b = '12345';
+// console.log(arrObj.b);
+
+// console.log(arr[1]);
+// console.log(arrObj[1]);
+
+// // const obj = { a:1, b:2} объект
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// };
+
+// const storeName = 'Rozetka';
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ['Tom', 'Jerry', 'Alex'],
+//   products: {
+//     phone: 1000,
+//     ipad: 700,
+//   },
+//   open: true,
+// };
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18 years old?', '18');
+// console.log(answer + 10);
+
+// const answers = [];
+// answers[0] = prompt('what is your first name?', '');
+// answers[1] = prompt('what is your last name?', '');
+// answers[2] = prompt('what old are you?', '');
+
+// console.log(typeof answers);
+
+// const category = 'toys';
+
+// // console.log(' https://someurl.com/' + category + '/' + '4' );
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+// console.log('arr' + '- object');
+// console.log(4 + +'- object');
+
+// console.log(4 + +'5');
+
+// let incr = 10,
+//   decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// // console.log(++incr);
+// // console.log(--decr);
+
+// // console.log(5 % 2);
+
+// console.log(2 + 2 * 2 !== '6');
+
+// // const isChecked = false;
+// // isClose = false;
+
+// // console.log(isChecked || !isClose);
